010a1f2368b0caf5922ef8ee25e719f775d74ab5
Nicholas Sheppard <nicholas@intersect.org.au>
2012-11-12 16:01:21 +1100

    Modified server admin config to work with Intersect servers

--- specchio-system-package/specchio_admin/config.php
+++ specchio-system-package/specchio_admin/config.php
@@ -4,72 +4,30 @@
  * ahueni
  */
  
- $host = 2; // environment switch
  $site_title = "SPECCHIO Database Administrator Tools";
- 
- if($host == 0)
- {
-	 
-	 // web server settings
-	 $server_address = "localhost";
-	 $server_directory = "/specchio_web/specchio_admin";
-	 
-	 // mail settings
-	 $admin_email = "ahueni@geo.uzh.ch";
-	 
-	 // db server settings
-	 $port = 3306;
-	 $db_server = "specchio.geo.uzh.ch";	 
-	 $database="specchio"; // default database (master database)
-	 
-	 // schemas
-	 $schemas = array("specchio");
 
- }
- 
-if($host == 1)
- {
+ // web server settings
+$host = gethostname();
+if (strncmp($host, "gsw1-dc10", 9) == 0) {  
+	$server_address = $host . ".intersect.org.au";
+} else {
+	$server_address = "localhost";
+}
+$server_directory = "/specchio_web/specchio_admin";
 	 
-	 // web server settings
-	 $server_address = "specchio.geo.uzh.ch";
-	 $server_directory = "/prod/specchio_admin";
+// mail settings
+$admin_email = "nicholas@intersect.org.au";
 	 
-	 // mail settings
-	 $admin_email = "ahueni@geo.uzh.ch";
+// db server settings
+$port = 3306;
+$db_server = "localhost";	 
+$database="specchio"; // default database (master database)
 	 
-	 // db server settings
-	 $port = 3306;
-	 $db_server = "specchio.geo.uzh.ch";	 
-	 $database="specchio"; // default database (master database)
-	 
-	 // schemas
-	 $schemas = array("specchio");
+// schemas
+$schemas = array("specchio");
 
- }
- 
- if($host == 2)
- {
-	 
-	 // web server settings
-	 $server_address = "localhost";
-	 $server_directory = "/specchio_web/specchio_admin";
-	 
-	 // mail settings
-	 $admin_email = "ahueni@geo.uzh.ch";
-	 
-	 // db server settings
-	 $port = 3306;
-	 $db_server = "localhost";	 
-	 $database="specchio"; // default database (master database)
-	 
-	 // schemas
-	 $schemas = array("specchio");
-
- }
- 
- 
- 
- $common_folder =  $_SERVER['DOCUMENT_ROOT'] . "$server_directory/common";
- $common_folder_http =  "http://$server_address$server_directory/common";
+  
+$common_folder =  $_SERVER['DOCUMENT_ROOT'] . "$server_directory/common";
+$common_folder_http =  "http://$server_address$server_directory/common";
  
 ?>
