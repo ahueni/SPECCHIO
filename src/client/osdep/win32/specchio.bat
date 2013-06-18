40289622076b8828d49aa237a83c7cf999638622
Nicholas Sheppard <nicholas@intersect.org.au>
2013-06-18 11:02:52 +1000

    Quoted paths used in the start-up scripts so that path names containing space will work correctly.

--- src/client/osdep/win32/specchio.bat
+++ src/client/osdep/win32/specchio.bat
@@ -3,4 +3,4 @@ IF NOT "%SPECCHIO_HOME%"=="" GOTO start
 SET SPECCHIO_HOME=%{INSTALL_PATH}
 
 :start
-start /d %SPECCHIO_HOME% /min java -jar -Dncsa.hdf.hdf5lib.H5.hdf5lib=%SPECCHIO_HOME%\win32\jhdf5.dll specchio-client.jar
+start /d "%SPECCHIO_HOME%" /min java -jar "-Dncsa.hdf.hdf5lib.H5.hdf5lib=%SPECCHIO_HOME%\win32\jhdf5.dll" specchio-client.jar
