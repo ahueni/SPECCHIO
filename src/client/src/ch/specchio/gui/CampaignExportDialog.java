5acb6298972b8f412887c4869797048b6b69047c
ahueni <ahueni@geo.uzh.ch>
2013-11-25 17:13:38 +0100

    Bugfix to make the dialog close when data are exported.

--- src/client/src/ch/specchio/gui/CampaignExportDialog.java
+++ src/client/src/ch/specchio/gui/CampaignExportDialog.java
@@ -164,6 +164,7 @@ public class CampaignExportDialog   extends JFrame implements ActionListener
 	    	}
 
 			this.setVisible(false);
+			this.dispose();
 			
 		} 	 
 		if(e.getActionCommand().equals("browse"))
