2c8d39a23668ee081a8131638fd49478ea09d4f9
ahueni <ahueni@geo.uzh.ch>
2015-05-11 21:43:45 +0200

    New class for datalink support

---
+++ src/client/src/ch/specchio/metadata/MD_EAV_Link_Field.java
@@ -0,0 +1,13 @@
+package ch.specchio.metadata;
+
+import ch.specchio.types.ConflictInfo;
+import ch.specchio.types.MetaParameter;
+
+public class MD_EAV_Link_Field extends MD_EAV_Field {
+
+	public MD_EAV_Link_Field(MetaParameter mp, ConflictInfo conflictInfo) {
+		super(mp, conflictInfo);
+		// TODO Auto-generated constructor stub
+	}
+
+}
