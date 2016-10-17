181d8d10e106068ef3f9cdbd0be70a749899c764
ahueni <ahueni@geo.uzh.ch>
2016-10-17 14:54:28 +0200

    Upgrade for new attribute: 'Acquisition Time (UTC)'

---
+++ conf/mysql/specchio_database_upgrade_V3.2.0_V3.2.1.sql
@@ -0,0 +1,6 @@
+-- UTC Time Attribute
+
+INSERT INTO `attribute`(`name`, `category_id`, `default_storage_field`, `description`) VALUES ('Acquisition Time (UTC)', (select category_id from category where name = 'General'), 'datetime_val', 'UTC time of data capture');
+
+INSERT INTO `attribute`(`name`, `category_id`, `default_storage_field`, `description`, `cardinality`) values('UTC Time Computation', (select category_id from category where name = 'Processing'), 'string_val', 'Notes produced by the SPECCHIO UTC computation routine', NULL);
+
