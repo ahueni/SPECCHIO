671115751cf3f2d3b4ad01d74d4356c7db5d7395
ahueni <ahueni@geo.uzh.ch>
2016-11-02 10:37:35 +0100

    New Waypoint ID attribute, designed to be used in connection with GPS waypoint ids.

--- conf/mysql/specchio_database_upgrade_V3.2.0_V3.2.1.sql
+++ conf/mysql/specchio_database_upgrade_V3.2.0_V3.2.1.sql
@@ -4,3 +4,6 @@ INSERT INTO `attribute`(`name`, `category_id`, `default_storage_field`, `descrip
 
 INSERT INTO `attribute`(`name`, `category_id`, `default_storage_field`, `description`, `cardinality`) values('UTC Time Computation', (select category_id from category where name = 'Processing'), 'string_val', 'Notes produced by the SPECCHIO UTC computation routine', NULL);
 
+-- Waypoint ID Attribute
+
+INSERT INTO `attribute`(`name`, `category_id`, `default_storage_field`, `description`) VALUES ('Waypoint ID', (select category_id from category where name = 'Location'), 'string_val', 'Name or ID of a waypoint');
