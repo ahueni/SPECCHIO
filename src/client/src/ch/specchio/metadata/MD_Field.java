6ce55e2aee44ad29468ff9ff3b856fa0deb581f5
ahueni <ahueni@geo.uzh.ch>
2017-09-12 17:32:44 +0200

    Refactoring for conflict info

--- src/client/src/ch/specchio/metadata/MD_Field.java
+++ src/client/src/ch/specchio/metadata/MD_Field.java
@@ -6,7 +6,9 @@ import ch.specchio.types.ConflictInfo;
 
 public abstract class MD_Field implements Comparable<MD_Field> {
 	
-	ConflictInfo conflict;
+	protected ConflictInfo conflict;
+
+
 	private Object new_value;
 	private String description; // help string
 	private String annotation = null; // currently used for binary content
@@ -73,7 +75,13 @@ public abstract class MD_Field implements Comparable<MD_Field> {
 		this.annotation = annotation;
 	}	
 
-	
+	public ConflictInfo getConflict() {
+		return conflict;
+	}
+
+	public void setConflict(ConflictInfo conflict) {
+		this.conflict = conflict;
+	}	
 	
 //	public boolean matches_db_field(String field){
 //		return false;
