cb5ac40549db87c64be22b9a423ca715a5d1de83
ahueni <ahueni@geo.uzh.ch>
2017-09-12 17:40:32 +0200

    Added JAXB annotation to help with marshalling

--- src/types/src/ch/specchio/jaxb/XmlMetaParameterValue.java
+++ src/types/src/ch/specchio/jaxb/XmlMetaParameterValue.java
@@ -2,11 +2,16 @@ package ch.specchio.jaxb;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+import ch.specchio.types.ArrayListWrapper;
+import ch.specchio.types.MetaDate;
 
 /**
  * XML-serialisable wrapper for meta-parameter values.
  */
 @XmlRootElement(name="metaparameter_value")
+@XmlSeeAlso({ArrayListWrapper.class})
 public class XmlMetaParameterValue {
 
 	@XmlElement(name="object") public Object object;
