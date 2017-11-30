8dfc7fe0ec75ff9b306aaf2739e6842b392e8719
ahueni <ahueni@geo.uzh.ch>
2017-11-30 13:46:00 +0100

    Class to display hierarchy data in the metadata editor

---
+++ src/client/src/ch/specchio/gui/HierarchyMetadataPanel.java
@@ -0,0 +1,20 @@
+package ch.specchio.gui;
+
+import java.awt.Frame;
+
+import ch.specchio.client.SPECCHIOClient;
+import ch.specchio.metadata.MDE_Controller;
+
+public class HierarchyMetadataPanel extends SpectrumMetadataPanel {
+
+	public HierarchyMetadataPanel(Frame owner, SPECCHIOClient specchioClient, MDE_Controller mdec) {
+		super(owner, specchioClient, mdec);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
