73262bde0527e7b54978eb87120084eb8f607f5d
ahueni <ahueni@geo.uzh.ch>
2017-11-30 12:13:48 +0100

    Updates that may help to better handle exceptions at the client side

--- src/webapp/src/ch/specchio/factories/SPECCHIOFactoryException.java
+++ src/webapp/src/ch/specchio/factories/SPECCHIOFactoryException.java
@@ -1,6 +1,8 @@
 package ch.specchio.factories;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 public class SPECCHIOFactoryException extends WebApplicationException {
 
@@ -42,4 +44,36 @@ public class SPECCHIOFactoryException extends WebApplicationException {
 		this.message = message;
 	}
 
+	
+    public SPECCHIOFactoryException() {
+    }
+
+    public SPECCHIOFactoryException(int status) {
+        super(status);
+    }
+
+    public SPECCHIOFactoryException(Response response) {
+        super(response);
+    }
+
+    public SPECCHIOFactoryException(Status status) {
+        super(status);
+    }
+
+    public SPECCHIOFactoryException(String message, Response response) {
+        super(message, response);
+    }
+
+    public SPECCHIOFactoryException(int status, String message) {
+        super(message, Response.status(Response.Status.BAD_REQUEST).entity(message).build());
+    }
+
+    public SPECCHIOFactoryException(Status status, String message) {
+        this(status.getStatusCode(), message);
+    }
+
+//    public SPECCHIOFactoryException(String message) {
+//        this(500, message);
+//    }	
+	
 }
