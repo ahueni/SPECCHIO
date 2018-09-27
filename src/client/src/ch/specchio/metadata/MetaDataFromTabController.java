3f3f4736e206dabb971303fae14267262bc74869
ahueni <ahueni@geo.uzh.ch>
2018-09-27 13:11:53 +0200

    Improved exception handling. Added spatial support.

--- src/client/src/ch/specchio/metadata/MetaDataFromTabController.java
+++ src/client/src/ch/specchio/metadata/MetaDataFromTabController.java
@@ -23,6 +23,8 @@ import ch.specchio.types.MatlabAdaptedArrayList;
 import ch.specchio.types.MetaDate;
 import ch.specchio.types.MetaParameter;
 import ch.specchio.types.MetaParameterFormatException;
+import ch.specchio.types.MetaSpatialPoint;
+import ch.specchio.types.Point2D;
 import ch.specchio.types.attribute;
 
 import jxl.Workbook;
@@ -44,129 +46,216 @@ public class MetaDataFromTabController  {
 			progressMonitor = progressMonitorIn;
 			
 		}
-		
+
 		@Override
 		public Integer doInBackground() throws SPECCHIOClientException {
 
 			int progress = 0;
-			
+
 			setProgress(progress);
+
+
+			// insert data
+			// insert each meta element separately
+			//
+			ArrayList<Integer> assigned_cols = model.getColumnNumbersOfAssignedColumns();
+
+			progressMonitor.setMaximum( assigned_cols.size());
+			progressMonitor.setMillisToDecideToPopup(0);
+			progressMonitor.setMillisToPopup(0);
+
+			Hashtable<Integer, Integer> LUT = model.getMatchingLUT();				
+
+			int col_index = 0;
+			int delta_progress = 1;
+			int attr_count = 0;
+			boolean spatial_attribute_present = false;
+			int lat_col = -1;
+			int lon_col = -1;
+			ArrayList<Point2D> spatial_positions = new ArrayList<Point2D>();
 			
-			
-				// insert data
-				// insert each meta element separately
-				ArrayList<Integer> assigned_cols = model.getColumnNumbersOfAssignedColumns();
+			// pre-scan for spatial attribute handling
+			while (col_index < assigned_cols.size())
+			{	
+				Integer assigned_col = assigned_cols.get(col_index);
+				attribute attr = model.getAttributeOfColumn(assigned_cols.get(col_index));
 				
-				progressMonitor.setMaximum( assigned_cols.size());
-				progressMonitor.setMillisToDecideToPopup(0);
-				progressMonitor.setMillisToPopup(0);
-
-				Hashtable<Integer, Integer> LUT = model.getMatchingLUT();				
-
-				int col_index = 0;
-				int delta_progress = 1;
-				int attr_count = 0;
+				String col_name = model.getItem_name_of_column(assigned_col);
 				
-
-				while (col_index < assigned_cols.size()  && !isCancelled()) 
+				if(model.getItem_name_of_column(assigned_col).equals("Spatial Position (Lon)"))
 				{
-
-					Integer assigned_col = assigned_cols.get(col_index);
-					attribute attr = model.getAttributeOfColumn(assigned_col);
-					ArrayList<Object> table_values = model.getValuesOfTableColumn(assigned_col);
-
-					progressMonitor.setNote("Inserting " + attr.getName());
-
-					int decision = MetaDataFromTabView.INSERT;
-					
-					// check that the type of the table column is compatible with the type of the attribute
-					Object sample_table_value = null;
-					Iterator<Object> iter = table_values.iterator();
-					while (sample_table_value == null && iter.hasNext()) {
-						sample_table_value = iter.next();
-					}
-					if (sample_table_value != null) {
-						if (attribute.INT_VAL.equals(attr.default_storage_field) && !(sample_table_value instanceof Number)) {
-							decision = view.get_user_decision_on_mismatched_type(attr, sample_table_value);
-						} else if (attribute.DOUBLE_VAL.equals(attr.default_storage_field) && !(sample_table_value instanceof Number)) {
-							decision = view.get_user_decision_on_mismatched_type(attr, sample_table_value);
-						} else if (attribute.DATETIME_VAL.equals(attr.default_storage_field) && !(sample_table_value instanceof Date)) {
-							decision = view.get_user_decision_on_mismatched_type(attr, sample_table_value);
-						}
-					}
-
-					if (decision == MetaDataFromTabView.INSERT) {
-						// check for attribute existance and warn user if exists for some spectra
-						ArrayList<Integer> spectrum_ids = new  ArrayList<Integer>();				
-						for(int index : LUT.keySet())
-						{
-							spectrum_ids.add(model.getSpectrum_ids().get(index));			
-						}
-						int existing_count = check_attribute_existance(attr, spectrum_ids);
-						if(existing_count > 0)
-						{
-							decision = view.get_user_decision_on_existing_fields(attr);
-							if(decision == MetaDataFromTabView.DELETE_EXISTING_AND_INSERT_NEW)
-							{						
-								specchio_client.removeEavMetadata(attr, spectrum_ids);
-								decision = MetaDataFromTabView.INSERT;					
-							}
-						}
-					}
-
-					if(decision == MetaDataFromTabView.INSERT)
-					{
-						Enumeration<Integer> li = LUT.keys();
-
-						// loop over all spectra in matching LUT
-						while(li.hasMoreElements())
-						{
-							Integer spectrum_index = li.nextElement();
-
-							Integer spectrum_id = model.getSpectrum_ids().get(spectrum_index);
-
-							Integer row = LUT.get(spectrum_index);
-							Object table_value = table_values.get(row);
-
-
-							// create new metaparameter for attribute
-							if(table_value != null && !table_value.equals(""))
-							{
-								try {
-									MetaParameter mp = MetaParameter.newInstance(attr);
-									mp.setValue(table_value);
-			
-									ArrayList<Integer> ids = new ArrayList<Integer>();
-									ids.add(spectrum_id);
-			
-									specchio_client.updateEavMetadata(mp, ids);
-									attr_count++;
-								}
-								catch (MetaParameterFormatException ex) {
-									// could not convert the table value to a meta-parameter of appropriate type
-									String message = "Skipped column " + (assigned_col+1) + ", row " + (row+1) + " : " + ex.getMessage();
-									progressMonitor.setNote(message);
-									System.err.println(message);
-								}
-							}
-							else
-							{
-								// empty value
-								// System.out.println("empty value at row " + row);
-							}
-
-						}
-
-					}
-					
-
-					progress += delta_progress;
-					setProgress(progress);
-					col_index++;
+					lon_col = assigned_col;
 				}
-				return attr_count;
-
+				
+				if(model.getItem_name_of_column(assigned_col).equals("Spatial Position (Lat)"))
+				{
+					lat_col = assigned_col;
+				}
+				
+				col_index++;
 			}
+
+			if(lat_col >=0 && lon_col >= 0)
+			{
+				spatial_attribute_present = true;
+				
+				ArrayList<Object> lon_values = model.getValuesOfTableColumn(lon_col);
+				ArrayList<Object> lat_values = model.getValuesOfTableColumn(lat_col);
+				
+				// compile spatial position records
+				Enumeration<Integer> li = LUT.keys();
+
+				// loop over all spectra in matching LUT
+				while(li.hasMoreElements())
+				{
+					Integer spectrum_index = li.nextElement();
+
+					Integer row = LUT.get(spectrum_index);
+					
+					if(lat_values.get(row) instanceof Number && lon_values.get(row) instanceof Number)
+					{
+						
+						double lat = 0; double lon = 0;
+						
+						if(lat_values.get(row) instanceof Integer)
+							lat = ((Integer) lat_values.get(row)).doubleValue();
+						else
+							lat = (Double) lat_values.get(row);
+
+						if(lon_values.get(row) instanceof Integer)
+							lon = ((Integer) lon_values.get(row)).doubleValue();
+						else
+							lon = (Double) lon_values.get(row);						
+						
+						
+						Point2D p = new Point2D(lat,lon);
+						
+						spatial_positions.add(p);
+					}
+					else
+					{
+						spatial_positions.add(null);
+					}
+
+				}
+				
+				// remove longitude from assigned columns (only one column needed to control insert of point)
+				assigned_cols.remove(new Integer(lon_col));
+
+				
+			}
+
+			col_index = 0;
+
+			while (col_index < assigned_cols.size()  && !isCancelled()) 
+			{
+
+				Integer assigned_col = assigned_cols.get(col_index);
+				attribute attr = model.getAttributeOfColumn(assigned_col);
+				ArrayList<Object> table_values = model.getValuesOfTableColumn(assigned_col);
+
+				progressMonitor.setNote("Inserting " + attr.getName());
+
+				int decision = MetaDataFromTabView.INSERT;
+
+				// check that the type of the table column is compatible with the type of the attribute
+				Object sample_table_value = null;
+				Iterator<Object> iter = table_values.iterator();
+				while (sample_table_value == null && iter.hasNext()) {
+					sample_table_value = iter.next();
+				}
+				if (sample_table_value != null) {
+					if (attribute.INT_VAL.equals(attr.default_storage_field) && !(sample_table_value instanceof Number)) {
+						decision = view.get_user_decision_on_mismatched_type(attr, sample_table_value);
+					} else if (attribute.DOUBLE_VAL.equals(attr.default_storage_field) && !(sample_table_value instanceof Number)) {
+						decision = view.get_user_decision_on_mismatched_type(attr, sample_table_value);
+					} else if (attribute.DATETIME_VAL.equals(attr.default_storage_field) && !(sample_table_value instanceof Date)) {
+						decision = view.get_user_decision_on_mismatched_type(attr, sample_table_value);
+					}
+				}
+
+				if (decision == MetaDataFromTabView.INSERT) {
+					// check for attribute existance and warn user if exists for some spectra
+					ArrayList<Integer> spectrum_ids = new  ArrayList<Integer>();				
+					for(int index : LUT.keySet())
+					{
+						spectrum_ids.add(model.getSpectrum_ids().get(index));			
+					}
+					int existing_count = check_attribute_existance(attr, spectrum_ids);
+					if(existing_count > 0)
+					{
+						decision = view.get_user_decision_on_existing_fields(attr);
+						if(decision == MetaDataFromTabView.DELETE_EXISTING_AND_INSERT_NEW)
+						{						
+							specchio_client.removeEavMetadata(attr, spectrum_ids, MetaParameter.SPECTRUM_LEVEL);
+							decision = MetaDataFromTabView.INSERT;					
+						}
+					}
+				}
+
+				if(decision == MetaDataFromTabView.INSERT)
+				{
+					Enumeration<Integer> li = LUT.keys();
+					int spat_ind = 0;
+
+					// loop over all spectra in matching LUT
+					while(li.hasMoreElements())
+					{
+						Integer spectrum_index = li.nextElement();
+
+						Integer spectrum_id = model.getSpectrum_ids().get(spectrum_index);
+
+						Integer row = LUT.get(spectrum_index);
+						Object table_value;
+						
+						if(attr.getName().equals("Spatial Position"))
+							table_value = spatial_positions.get(spat_ind++);
+						else
+							table_value = table_values.get(row);
+
+
+						// create new metaparameter for attribute
+						if(table_value != null && !table_value.equals(""))
+						{
+							try {
+								MetaParameter mp = MetaParameter.newInstance(attr);
+								
+								if(mp instanceof MetaSpatialPoint)
+									((MetaSpatialPoint) mp).setValue((Point2D) table_value);
+								else
+									mp.setValue(table_value);
+
+								ArrayList<Integer> ids = new ArrayList<Integer>();
+								ids.add(spectrum_id);
+
+								specchio_client.updateEavMetadata(mp, ids);
+								attr_count++;
+							}
+							catch (MetaParameterFormatException ex) {
+								// could not convert the table value to a meta-parameter of appropriate type
+								String message = "Skipped column " + (assigned_col+1) + ", row " + (row+1) + " : " + ex.getMessage();
+								progressMonitor.setNote(message);
+								System.err.println(message);
+							}
+						}
+						else
+						{
+							// empty value
+							// System.out.println("empty value at row " + row);
+						}
+
+					}
+
+				}
+
+
+				progress += delta_progress;
+				setProgress(progress);
+				col_index++;
+			}
+			return attr_count;
+
+		}
 
 		@Override
 		public void done() {
@@ -249,7 +338,7 @@ public class MetaDataFromTabController  {
 
 	public void setAttributeAtColumn(int col, combo_table_data itemAt) {
 			
-		model.setAttributeOfColumn(col, itemAt.id);
+		model.setAttributeOfColumn(col, itemAt.id, itemAt.toString());
 		
 	}
 
@@ -473,8 +562,14 @@ public class MetaDataFromTabController  {
 			
 			if(matching_attribute_name != null && !matching_attribute_name.equals("NIL"))
 			{
+				
+				String attribute_name = matching_attribute_name;
+				
+				if(attribute_name.equals("Spatial Position (Lon)") || attribute_name.equals("Spatial Position (Lat)"))
+					attribute_name = "Spatial Position";
+				
 				// try and find attribute in database
-				attribute attr = attr_hash.get(matching_attribute_name);
+				attribute attr = attr_hash.get(attribute_name);
 				
 				// assign category and element for current column
 				if(attr != null)
@@ -482,7 +577,7 @@ public class MetaDataFromTabController  {
 					Category cat = model.getCategories_hash().get(attr.cat_name);
 					
 					model.setCategoryOfColumn(cat, col);
-					model.setAttributeOfColumn(col, attr.getId());					
+					model.setAttributeOfColumn(col, attr.getId(), matching_attribute_name);					
 				}
 				
 				
