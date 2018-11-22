e6dcc37bd231d2553722b07945a5e42ce6e10455
ahueni <ahueni@geo.uzh.ch>
2018-11-22 13:09:31 +0100

    Added initialisations and default constructor that might be used by Jersey at a future stage (caused by a wrong object type used for point data in SpatialPosition metadata in the Flox Reader)

--- src/types/src/ch/specchio/types/spatial_pos.java
+++ src/types/src/ch/specchio/types/spatial_pos.java
@@ -1,9 +1,56 @@
 package ch.specchio.types;
 
+//import javax.xml.bind.annotation.XmlElement;
+//import javax.xml.bind.annotation.XmlRootElement;
+
+//@XmlRootElement(name="spatial_pos")
 public class spatial_pos
 {
-	public Double latitude = null;
-	public Double longitude = null;
-	public Double altitude = null;
+	public Double latitude = new Double(0);
+	public Double longitude = new Double(0);
+	public Double altitude = new Double(0);
 	public String location_name = "";
+	
+	public spatial_pos()
+	{
+		
+	}
+
+//	@XmlElement(name="latitude")
+//	public Double getLatitude() {
+//		return latitude;
+//	}
+
+//	public void setLatitude(Double latitude) {
+//		this.latitude = latitude;
+//	}
+//
+////	@XmlElement(name="longitude")
+//	public Double getLongitude() {
+//		return longitude;
+//	}
+//
+//	public void setLongitude(Double longitude) {
+//		this.longitude = longitude;
+//	}
+//
+////	@XmlElement(name="altitude")
+//	public Double getAltitude() {
+//		return altitude;
+//	}
+//
+//	public void setAltitude(Double altitude) {
+//		this.altitude = altitude;
+//	}
+//
+////	@XmlElement(name="location_name")
+//	public String getLocation_name() {
+//		return location_name;
+//	}
+//
+//	public void setLocation_name(String location_name) {
+//		this.location_name = location_name;
+//	}
+	
+	
 }
