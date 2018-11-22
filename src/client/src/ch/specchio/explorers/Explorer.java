c333cc8ca67e461410deaaf4673adeeb4c6859ca
ahueni <ahueni@geo.uzh.ch>
2018-11-22 13:01:55 +0100

    Unification of plotting tools with new Explorer base class

---
+++ src/client/src/ch/specchio/explorers/Explorer.java
@@ -0,0 +1,15 @@
+package ch.specchio.explorers;
+
+import javax.swing.JPanel;
+
+import ch.specchio.interfaces.ProgressReportInterface;
+
+public class Explorer extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected ProgressReportInterface pr;
+	public boolean plot_ready = false;
+
+}
